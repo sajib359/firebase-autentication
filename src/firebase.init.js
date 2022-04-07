1e58cd37abce9bc5efcdb7c9f3e43170b75248d7
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-07 23:16:32 +0600

    setup firebase bt not complete

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1WMPSG_ij1mIipEFc1-iPHhJtydLxP-c",
+  authDomain: "fir-authentication-9ed5a.firebaseapp.com",
+  projectId: "fir-authentication-9ed5a",
+  storageBucket: "fir-authentication-9ed5a.appspot.com",
+  messagingSenderId: "788863275117",
+  appId: "1:788863275117:web:05e61794903e460b7cef64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
