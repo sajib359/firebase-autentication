02f776b37c89753e80f4e7085a1830b2e2e1e02b
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-10 10:06:56 +0600

    popUp method enable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import app from './firebase.init';
-import {getAuth} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
 const auth =getAuth(app);
 function App() {
+  const provider = new GoogleAuthProvider();
+ const signInHandler =()=>{
+   signInWithPopup(auth,provider)
+   .then(result=>{
+     const user=result.user;
+     console.log(user);
+   })
+   .catch(error=>{
+    console.log('error',error);
+   })
+ }
   return (
     <div className="App">
-      
+      <button onClick={signInHandler}>Google sign In</button>
     </div>
   );
 }
