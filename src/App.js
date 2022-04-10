902cd980ca11c88b8aca8bf38ab8ecb92b7ea469
Sajib Hossain <sajib.sj359@gmail.com>
2022-04-10 11:30:46 +0600

    SignOut and toggle information

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import app from './firebase.init';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import { useState } from 'react';
 const auth =getAuth(app);
 function App() {
+  const [user , setUser]= useState({});
   const provider = new GoogleAuthProvider();
  const signInHandler =()=>{
    signInWithPopup(auth,provider)
    .then(result=>{
      const user=result.user;
+     setUser(user);
      console.log(user);
    })
    .catch(error=>{
-    console.log('error',error);
+    console.error('error',error);
    })
+
+ }
+ const signOutHandler =()=>{
+  signOut(auth)
+  .then(()=>{
+    setUser({});
+  })
+  .catch(()=>{
+    setUser({});
+  })
  }
   return (
     <div className="App">
-      <button onClick={signInHandler}>Google sign In</button>
+      {
+        user.email ? <button onClick={signOutHandler}>Sign Out</button>
+        :
+        <button onClick={signInHandler}>Google sign In</button>
+        
+      }
+      <h2>Name: {user.displayName}</h2>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
